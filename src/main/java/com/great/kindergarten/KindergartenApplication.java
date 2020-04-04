c141247995ff0e2e332413809f680cdd9a61b332
jake <1668403913@qq.com>
2020-04-04 14:38:49 +0800

    first commit

---
+++ src/main/java/com/great/kindergarten/KindergartenApplication.java
@@ -0,0 +1,18 @@
+package com.great.kindergarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JK
+ */
+@SpringBootApplication
+public class KindergartenApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KindergartenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/great/kindergarten/KindergartenApplicationTests.java
@@ -0,0 +1,15 @@
+package com.great.kindergarten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergartenApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
